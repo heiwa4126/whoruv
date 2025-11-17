4066118ca31881b3d1a73f3feb10b9753128f849
heiwa4126 <heiwa4126@gmail.com>
2025-11-17 18:01:37 +0900

    fix repository url

--- src/whoruv/__init__.py
+++ src/whoruv/__init__.py
@@ -3,4 +3,4 @@ from importlib.metadata import version
 from ._core import PythonInfo, whoruv
 
 __all__ = ["whoruv", "PythonInfo"]
-__version__ = version("whoruv")  # Python 3.9+ only
+__version__ = version(__package__ or __name__)  # Python 3.9+ only
