99c3088a6a5827c3da05a5f857a08b1260ee3ca5
heiwa4126 <heiwa4126@gmail.com>
2026-08-24 13:09:11 +0900

    update modules

--- src/whoruv/__init__.py
+++ src/whoruv/__init__.py
@@ -3,4 +3,4 @@ from importlib.metadata import version
 from ._core import PythonInfo, whoruv
 
 __version__ = version(__package__ or __name__)  # Python 3.9+ only
-__all__ = ["whoruv", "PythonInfo", "__version__"]
+__all__ = ["PythonInfo", "__version__", "whoruv"]
